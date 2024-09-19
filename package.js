ecb225052138e4d23e03e1dfa01fc2da575966ef
Jopro2994 <160973230+Jopro2994@users.noreply.github.com>
2024-09-19 12:18:10 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "counter-app",
+  "version": "1.0.0",
+  "description": "A simple counter app with Node.js, Express, and SQLite",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "sqlite3": "^5.0.2"
+  }
+}
